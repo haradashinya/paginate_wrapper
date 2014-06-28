7f4d67826f4eb1b801aa7059a05787956c3c62f1
okamurayasuyuki <haradashinya@gmail.com>
2014-06-28 22:30:20 +0900

    add example

--- example/app.py
+++ example/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template,request
 from paginate_wrapper import paginate
-app = Flask()
+app = Flask(__name__)
 
+# Each page has 10 items.
+PER_PAGE = 10
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "AAA"
+    _page = request.args.get("page",1)
+    page = int(_page)
+    all_items = ['item-'+str(item) for item in range(1,1000)]
+    items = all_items[PER_PAGE * (page- 1):PER_PAGE * page]
+
+    obj = paginate(offset=7,
+                   objects=all_items,
+                   page = page,
+                   per_page = PER_PAGE)
+
+    return render_template('index.html',
+                           items = items,
+                           obj =obj
+                           )
 
 if __name__ == '__main__':
     app.run(debug=True)
